3c076d82d965bd4d6ec0c2ee895d7ee2575a9f33
Shalveena Rohde <shalveena.s.prasad@gmail.com>
2021-08-11 10:36:46 +1000

    finish validation of search with Andrew Fulton of Portable

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,38 +1,47 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 const IndexPage = () => {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
-  
-  const doSearch = async() => {
-    const resultSet = await axios.get('/api/search', {
-      params: {
-        term: term
-      }
-    })
-    setResults(resultSet.data);
-  }
-  
+  const [validationmsg, setValidationmsg] = useState("");
+
+  const doSearch = async () => {
+    if (term.length <= 0) {
+      setValidationmsg("Please input search term");
+    } else {
+      const resultSet = await axios.get("/api/search", {
+        params: {
+          term: term,
+        },
+      });
+      setValidationmsg("");
+      setResults(resultSet.data);
+    }
+  };
+
   return (
     <div>
       <h1>Guardian Search</h1>
       <div>
-        <input
-          value={term}
-          onChange={(evt) => setTerm(evt.target.value)} />
+        <input value={term} onChange={(evt) => setTerm(evt.target.value)} />
         <button onClick={() => doSearch()}>Search</button>
+        <div>{validationmsg}</div>
       </div>
       <div>
         <h2>Results</h2>
         <ul>
-          { results.map(result => {
-            return <li><a href={result.url}>{result.title}</a></li>
+          {results.map((result) => {
+            return (
+              <li>
+                <a href={result.url}>{result.title}</a>
+              </li>
+            );
           })}
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default IndexPage;
